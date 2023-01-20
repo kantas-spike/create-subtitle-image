e6ca5c708aa90aaa88acca89b8bfcde832190953
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-01-21 01:09:33 +0900

    fix: base_dirをshell内で設定

--- create-subtitle-image.sh
+++ create-subtitle-image.sh
@@ -1,7 +1,8 @@
 DEFAULT_SVG=/Applications/Inkscape.app/Contents/Resources/share/inkscape/templates/default.svg
 INKSCAPE_PYTHON=/Applications/Inkscape.app/Contents/Resources/bin/python3
 SIMPINKSCR_SCRIPT=~/Library/Application\ Support/org.inkscape.Inkscape/config/inkscape/extensions/SimpInkScr/simpinkscr/simple_inkscape_scripting.py
+SCRIPT_DIR=$(dirname "$0")
 export DYLD_LIBRARY_PATH=/Applications/Inkscape.app/Contents/Resources/lib
 export PYTHONPATH=/Applications/Inkscape.app/Contents/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages:/Applications/Inkscape.app/Contents/Resources/share/inkscape/extensions:.
 
-${INKSCAPE_PYTHON} "${SIMPINKSCR_SCRIPT}" --py-source "./create-subtitle-image.py" "${DEFAULT_SVG}" -- "$@"
+${INKSCAPE_PYTHON} "${SIMPINKSCR_SCRIPT}" --py-source "${SCRIPT_DIR}/create-subtitle-image.py" "${DEFAULT_SVG}" -- --base-dir $(pwd) "$@"
