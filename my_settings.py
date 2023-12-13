f104f0ed88349d1acc667ccf0fc4c09815618d51
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-12-13 22:43:29 +0900

    tmp: ダミー実装追加

---
+++ my_settings.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+def read_config_file(path):
+    pass
---
+++ subtitle_creator.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+def run(srt_path, config_path, output_path):
+    print("run!!: ", srt_path, config_path, output_path)
